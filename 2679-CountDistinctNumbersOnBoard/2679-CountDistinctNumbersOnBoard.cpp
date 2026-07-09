6f3e8e0d0b61703db737d319403d963109eee52b
JananilakshmiM <jananilakshmim71@gmail.com>
2026-07-09 15:06:46 +0530

    Create 2679-CountDistinctNumbersOnBoard

---
+++ 2679-CountDistinctNumbersOnBoard/2679-CountDistinctNumbersOnBoard.cpp
@@ -0,0 +1,13 @@
+// Last updated: 09/07/2026, 15:06:45
+class Solution {
+public:
+    int distinctIntegers(int n) {
+        if(n == 1) {
+            return 1;
+        }
+        else {
+            return n - 1;
+        }
+        
+    }
+};
