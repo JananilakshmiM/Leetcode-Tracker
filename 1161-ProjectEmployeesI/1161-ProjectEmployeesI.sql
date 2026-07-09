d2eabbcc2525037bcbe009d998ef9f1b1b2e0f83
JananilakshmiM <jananilakshmim71@gmail.com>
2026-07-09 15:08:40 +0530

    Create 1161-ProjectEmployeesI

---
+++ 1161-ProjectEmployeesI/1161-ProjectEmployeesI.sql
@@ -0,0 +1,8 @@
+-- Last updated: 09/07/2026, 15:08:39
+SELECT 
+    p.project_id,
+    ROUND(AVG(e.experience_years), 2) AS average_years
+FROM Project p
+JOIN Employee e
+ON p.employee_id = e.employee_id
+GROUP BY p.project_id;
