a9b1090ea076ca7da5ac53e691b1cd0209ffd3fa
JananilakshmiM <jananilakshmim71@gmail.com>
2026-07-09 15:09:25 +0530

    Create 626-ExchangeSeats

---
+++ 626-ExchangeSeats/626-ExchangeSeats.sql
@@ -0,0 +1,10 @@
+-- Last updated: 09/07/2026, 15:09:25
+SELECT
+    CASE
+        WHEN id % 2 = 1 AND id = (SELECT MAX(id) FROM Seat) THEN id
+        WHEN id % 2 = 1 THEN id + 1
+        ELSE id - 1
+    END AS id,
+    student
+FROM Seat
+ORDER BY id;
