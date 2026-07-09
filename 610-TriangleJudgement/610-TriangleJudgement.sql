f29951517761e92f5d9c58c7c9920558d589360b
JananilakshmiM <jananilakshmim71@gmail.com>
2026-07-09 15:09:31 +0530

    Create 610-TriangleJudgement

---
+++ 610-TriangleJudgement/610-TriangleJudgement.sql
@@ -0,0 +1,10 @@
+-- Last updated: 09/07/2026, 15:09:30
+SELECT 
+    x,
+    y,
+    z,
+    CASE 
+        WHEN x + y > z AND x + z > y AND y + z > x THEN 'Yes'
+        ELSE 'No'
+    END AS triangle
+FROM Triangle;
