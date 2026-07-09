c1cb0425f8efd0e7f37213cc71881734cb7f5752
JananilakshmiM <jananilakshmim71@gmail.com>
2026-07-09 15:06:06 +0530

    Create 3636-CheckBalancedString

---
+++ 3636-CheckBalancedString/3636-CheckBalancedString.java
@@ -0,0 +1,19 @@
+// Last updated: 09/07/2026, 15:06:06
+class Solution {
+    public boolean isBalanced(String num) {
+        int evenSum = 0;
+        int oddSum = 0;
+
+        for (int i = 0; i < num.length(); i++) {
+            int digit = num.charAt(i) - '0';
+
+            if (i % 2 == 0) {
+                evenSum += digit;
+            } else {
+                oddSum += digit;
+            }
+        }
+
+        return evenSum == oddSum;
+    }
+}
