028905d8555fa7eeaf42c8ff1740b41ac596b98d
Jananilakshmi M <jananilakshmim71@gmail.com>
2026-08-02 09:04:57 +0530

    Create

---
+++ problem-1785641696300/version/java/problem-1785641696300_20260802_090456.java
@@ -0,0 +1,26 @@
+// Last updated: 02/08/2026, 09:04:56
+1class Solution {
+2    public long maxPairStrength(int[] nums) {
+3        long max=0;
+4        int n=nums.length;
+5
+6        for(int i=0; i<n; i++){
+7            for(int j=i+1; j<n; j++){
+8                long val1=nums[i];
+9                long val2=nums[j];
+10                long g=gcd(val1,val2);
+11                long s=(val1*val2)/(g*g);
+12                max=Math.max(max,s);
+13            }
+14        }
+15        return max;
+16    }
+17    private long gcd(long a, long b){
+18        while(b!=0){
+19            long temp=b;
+20            b=a%b;
+21            a=temp;
+22        }
+23        return a;
+24    }
+25}
