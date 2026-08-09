2e9fd4b5b89a4ec609eabab30818932d5e2f963b
Jananilakshmi M <jananilakshmim71@gmail.com>
2026-08-09 09:24:45 +0530

    Create

---
+++ problem-1786247684218/version/java/problem-1786247684218_20260809_092444.java
@@ -0,0 +1,22 @@
+// Last updated: 09/08/2026, 09:24:44
+1class Solution {
+2    public double minPrice(int[] prices, int[] discounts) {
+3        Arrays.sort(prices);
+4        Arrays.sort(discounts);
+5        double total=0;
+6        int pIndex=prices.length-1;
+7        int dIndex=discounts.length-1;
+8        while(pIndex>=0&&dIndex>=0){
+9            double discountPercent=discounts[dIndex];
+10            total+=prices[pIndex]*(100.0-discountPercent)/100.0;
+11            pIndex--;
+12            dIndex--;
+13        }
+14        while(pIndex>=0){
+15            total+=prices[pIndex];
+16            pIndex--;
+17        }
+18        return total;
+19        
+20    }
+21}
